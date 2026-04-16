73fae8bd35e0e5bb33d994c65827bda513595428
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2026-04-16 14:12:46 +0000

    fix: adjust base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   
-  base: "/Cinema_Seat_Manager_astrid/", 
+  base: "/Cinema_Seat_Manager_astrid", 
   build: {
     outDir: "dist", 
   }
