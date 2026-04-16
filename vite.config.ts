e172d90017b28aefc6909baa1b2d1a2bcb480c08
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2026-04-16 14:00:37 +0000

    fix: Deploy paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/Cinema_Seat_Manager_astrid/"
+  base: './', 
+  build: {
+    outDir: 'dist',
+  }
 });
